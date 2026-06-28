22b728fc314b16c9f05f4a994ecb1775da555f8c
Long 2k6 <skullmuder@gmail.com>
2026-06-28 16:31:34 +0700

    update

--- src/core/Scene.cpp
+++ src/core/Scene.cpp
@@ -30,11 +30,20 @@ namespace Long {
 			}
 		};
 		copy_component.operator() < Name > ();
+		copy_component.operator() < Transform > ();  
 		copy_component.operator() < MatrixTransform > ();
 		copy_component.operator() < WorldAABB > ();
 		copy_component.operator() < MeshFilter > ();
 		copy_component.operator() < MeshRenderer > ();
 		copy_component.operator() < BoxCollider3D > ();
-		copy_component.operator() < Hierarchy > ();
+		for (auto [e, h] : src.view<Hierarchy>().each()) {
+			Hierarchy copy;
+			copy.parent = (h.parent == entt::null) ? entt::null : map_entt[h.parent];
+			copy.children.reserve(h.children.size());
+			for (entt::entity child : h.children) {
+				copy.children.push_back(map_entt[child]);
+			}
+			m_registry.emplace<Hierarchy>(map_entt[e], std::move(copy));
+		}
 	}
 } // namespace Long
