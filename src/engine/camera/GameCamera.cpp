01d2dbdbfd1bb3f7e8ca88745b48eda1529ce5c9
Long 2k6 <skullmuder@gmail.com>
2026-06-30 23:22:04 +0700

    UPDATE

--- src/engine/camera/GameCamera.cpp
+++ src/engine/camera/GameCamera.cpp
@@ -4,10 +4,10 @@ namespace Long {
 	GameCamera::GameCamera()
 	{
 		static raylib::Vector3 default_target{ 0.0f,0.0f,0.0f }; 
-		static const float fov = 100.0f; 
-		m_camera.position = { 6.0f,6.0f,6.0f }; 
+		static const float fov = 45.0f; 
+		m_camera.position = { 6.0f,12.0f,6.0f }; 
 		m_camera.fovy = fov; 
-		m_camera.projection = ::CAMERA_ORTHOGRAPHIC;
+		m_camera.projection = ::CAMERA_PERSPECTIVE;
 		m_camera.SetTarget(default_target); 
 		SetClip(0.3f, 50.0f);
 	}
